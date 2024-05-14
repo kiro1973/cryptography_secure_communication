db1660772845a749523cee3ebfcd13e5530246fa
Shady Osama <shadyosama658@gmail.com>
2024-05-14 18:41:27 +0200

    added authen

--- client111.py
+++ client111.py
@@ -8,8 +8,56 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import os
-from utils.aes import  AESDecryptor
+from utils.aes import AESDecryptor
 from utils.hashing import Hashing
+import hashlib
+
+
+# Function to hash password
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
+
+# Function for client registration
+def register(username, password):
+    hashed_password = hash_password(password)
+    request_data = {
+        "action": "register",
+        "username": username,
+        "password": password,
+        "hashed_password": hashed_password,
+    }
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(("localhost", 43445))
+        client.send(json.dumps(request_data).encode())
+        response = client.recv(1024).decode()
+        print(response)
+        return response
+
+
+# Function for client login
+def login(username, password):
+    request_data = {"action": "login", "username": username, "password": password}
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(("localhost", 43445))
+        client.send(json.dumps(request_data).encode())
+        response = client.recv(1024).decode()
+        print(response)
+        return response
+
+
+login_response = None
+print("1. Register")
+print("2. Login")
+choice = input("Enter your choice (1/2/3): ")
+if choice == "1":
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    login_response = register(username, password)
+elif choice == "2":
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    login_response = login(username, password)
 # Create a socket object
 s = socket.socket()
 
@@ -22,7 +70,7 @@ print("Socket successfully created")
 port = 12345
 
 # Bind to the port
-s.bind(('', port))
+s.bind(("", port))
 print("Socket binded to %s" % (port))
 
 # Put the socket into listening mode
@@ -34,74 +82,82 @@ parameters = dh.generate_parameters(generator=2, key_size=2048)
 server_private_key = parameters.generate_private_key()
 
 while True:
-    # Establish connection with client
-    c, addr = s.accept()
-    print('Got connection from', addr)
-    
-    # Serialize parameters and server public key
-    parameters_bytes = parameters.parameter_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.ParameterFormat.PKCS3
-    )
-    server_public_key = server_private_key.public_key().public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    data = {
-        'parameters': parameters_bytes.decode(),
-        'server_public_key': server_public_key.decode()
-    }
 
-    # Send JSON data to the client
-    c.send(json.dumps(data).encode())
-  
-    # Receive data from the client
-    data = c.recv(4096)
-    if not data:
-        break
-    print("Client Public Key :", data.decode())
- 
-    # Load the PEM-encoded DH public key directly as a DHPublicKey object
-    client_public_key = serialization.load_pem_public_key(data, backend=default_backend())
- 
-    shared_key = server_private_key.exchange(client_public_key)
-   
-    # Perform key derivation.
-    derived_key = HKDF(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=None,
-        info=b'handshake data',
-    ).derive(shared_key)
+    if login_response == "Successfully logged in" or "Registered successfully":
+        # Establish connection with client
+        c, addr = s.accept()
+        print("Got connection from", addr)
 
-    print("Derived Key (server):", derived_key.hex())
+        # Serialize parameters and server public key
+        parameters_bytes = parameters.parameter_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.ParameterFormat.PKCS3,
+        )
+        server_public_key = server_private_key.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        )
+        data = {
+            "parameters": parameters_bytes.decode(),
+            "server_public_key": server_public_key.decode(),
+        }
 
-    # Receive encrypted message from client
-    #encrypted_message = c.recv(4096)
-    # Receive the JSON data
-    json_data = b''
-    while True:
-        chunk = c.recv(4096)
-        if not chunk:
+        # Send JSON data to the client
+        c.send(json.dumps(data).encode())
+
+        # Receive data from the client
+        data = c.recv(4096)
+        if not data:
             break
-        json_data += chunk
-    print("json_data",json_data)
-    # Deserialize the JSON data
-    aes_encrypted_base64 = json.loads(json_data.decode())
+        print("Client Public Key :", data.decode())
 
-    # Decode each base64 string back to bytes
-    aes_encrypted = tuple(base64.b64decode(part.encode()) for part in aes_encrypted_base64)
-    # json_data = s.recv(4096)
-    # aes_encrypted = json.loads(json_data.decode())
-    # Decrypt the message using AES
-    # Decrypt the message
-    aes_decryptor = AESDecryptor(derived_key)
-    #aes_decrypted_parts = [aes_decryptor.decrypt(part) for part in aes_encrypted]
+        # Load the PEM-encoded DH public key directly as a DHPublicKey object
+        client_public_key = serialization.load_pem_public_key(
+            data, backend=default_backend()
+        )
 
-    aes_decrypted = aes_decryptor.decrypt(*aes_encrypted)
-    print("aes_decrypted",aes_decrypted)
-    # Close the connection with the client
-    c.close()
+        shared_key = server_private_key.exchange(client_public_key)
+
+        # Perform key derivation.
+        derived_key = HKDF(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=None,
+            info=b"handshake data",
+        ).derive(shared_key)
+
+        print("Derived Key (server):", derived_key.hex())
+
+        # Receive encrypted message from client
+        # encrypted_message = c.recv(4096)
+        # Receive the JSON data
+        json_data = b""
+        while True:
+            chunk = c.recv(4096)
+            if not chunk:
+                break
+            json_data += chunk
+        print("json_data", json_data)
+        # Deserialize the JSON data
+        aes_encrypted_base64 = json.loads(json_data.decode())
+
+        # Decode each base64 string back to bytes
+        aes_encrypted = tuple(
+            base64.b64decode(part.encode()) for part in aes_encrypted_base64
+        )
+        # json_data = s.recv(4096)
+        # aes_encrypted = json.loads(json_data.decode())
+        # Decrypt the message using AES
+        # Decrypt the message
+        aes_decryptor = AESDecryptor(derived_key)
+        # aes_decrypted_parts = [aes_decryptor.decrypt(part) for part in aes_encrypted]
+
+        aes_decrypted = aes_decryptor.decrypt(*aes_encrypted)
+        print("aes_decrypted", aes_decrypted)
+        # Close the connection with the client
+        c.close()
+    else:
+        pass
 
 # Close the server socket
 s.close()
--- client222.py
+++ client222.py
@@ -10,71 +10,124 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from utils.aes import AESEncryptor
 from utils.hashing import Hashing
 import os
-
-# Create a socket object
-s = socket.socket()
-
-# Connect to the server
-s.connect(('127.0.0.1', 12345))
-
-# Receive data from the server
-data = s.recv(4096)
-received_data = json.loads(data)
-
-# Extract parameters and server's public key
-parameters_bytes = received_data['parameters'].encode()
-server_public_key_bytes = received_data['server_public_key'].encode()
-
-# Deserialize server's public key and DH parameters
-server_public_key = serialization.load_pem_public_key(server_public_key_bytes, backend=default_backend())
-parameters = serialization.load_pem_parameters(parameters_bytes, backend=default_backend())
-
-# Generate client private key
-client_private_key = parameters.generate_private_key()
-
-# Send client's public key to the server
-s.sendall(client_private_key.public_key().public_bytes(
-    encoding=serialization.Encoding.PEM,
-    format=serialization.PublicFormat.SubjectPublicKeyInfo
-))
-
-# Exchange keys
-shared_key = client_private_key.exchange(server_public_key)
-
-# Perform key derivation.
-derived_key = HKDF(
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=None,
-    info=b'handshake data',
-).derive(shared_key)
-
-print("Derived Key (client):", derived_key.hex())
-
-# Encrypt the message using AES
-message = b"Hello from client!"
-aes_encryptor = AESEncryptor(derived_key)
-aes_encrypted = aes_encryptor.encrypt(message)
-print("aes_encrypted tuple ",aes_encrypted)
-# Convert bytes to hexadecimal strings
-#aes_encrypted_hex = [part.hex() for part in aes_encrypted]
-
-# Convert the list of hexadecimal strings to JSON
-#json_data = json.dumps(aes_encrypted).encode()
-
-aes_encrypted_base64 = [base64.b64encode(part).decode() for part in aes_encrypted]
-
-# Convert the list of base64-encoded strings to JSON
-json_data = json.dumps(aes_encrypted_base64).encode()
+import hashlib
 
 
+# Function to hash password
+def hash_password(password):
+    return hashlib.sha256(password.encode()).hexdigest()
 
-# Send the JSON data over the network
-s.sendall(json_data)
-#print("aes_encrypted",*aes_encrypted)
 
-# Send the encrypted message to the server
-#s.sendall(*aes_encrypted)
+# Function for client registration
+def register(username, password):
+    hashed_password = hash_password(password)
+    request_data = {
+        "action": "register",
+        "username": username,
+        "password": password,
+        "hashed_password": hashed_password,
+    }
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(("localhost", 43445))
+        client.send(json.dumps(request_data).encode())
+        response = client.recv(1024).decode()
+        print(response)
+        return response
 
-# Close the connection with the server
-s.close()
+
+# Function for client login
+def login(username, password):
+    request_data = {"action": "login", "username": username, "password": password}
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
+        client.connect(("localhost", 43445))
+        client.send(json.dumps(request_data).encode())
+        response = client.recv(1024).decode()
+        print(response)
+        return response
+
+
+login_response = None
+print("1. Register")
+print("2. Login")
+choice = input("Enter your choice (1/2/3): ")
+if choice == "1":
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    login_response = register(username, password)
+elif choice == "2":
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    login_response = login(username, password)
+
+if login_response == "Successfully logged in" or "Registered successfully":
+    # Create a socket object
+    s = socket.socket()
+
+    # Connect to the server
+    s.connect(("127.0.0.1", 12345))
+
+    # Receive data from the server
+    data = s.recv(4096)
+    received_data = json.loads(data)
+
+    # Extract parameters and server's public key
+    parameters_bytes = received_data["parameters"].encode()
+    server_public_key_bytes = received_data["server_public_key"].encode()
+
+    # Deserialize server's public key and DH parameters
+    server_public_key = serialization.load_pem_public_key(
+        server_public_key_bytes, backend=default_backend()
+    )
+    parameters = serialization.load_pem_parameters(
+        parameters_bytes, backend=default_backend()
+    )
+
+    # Generate client private key
+    client_private_key = parameters.generate_private_key()
+
+    # Send client's public key to the server
+    s.sendall(
+        client_private_key.public_key().public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo,
+        )
+    )
+
+    # Exchange keys
+    shared_key = client_private_key.exchange(server_public_key)
+
+    # Perform key derivation.
+    derived_key = HKDF(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=None,
+        info=b"handshake data",
+    ).derive(shared_key)
+
+    print("Derived Key (client):", derived_key.hex())
+
+    # Encrypt the message using AES
+    message = b"Hello from client!"
+    aes_encryptor = AESEncryptor(derived_key)
+    aes_encrypted = aes_encryptor.encrypt(message)
+    print("aes_encrypted tuple ", aes_encrypted)
+    # Convert bytes to hexadecimal strings
+    # aes_encrypted_hex = [part.hex() for part in aes_encrypted]
+
+    # Convert the list of hexadecimal strings to JSON
+    # json_data = json.dumps(aes_encrypted).encode()
+
+    aes_encrypted_base64 = [base64.b64encode(part).decode() for part in aes_encrypted]
+
+    # Convert the list of base64-encoded strings to JSON
+    json_data = json.dumps(aes_encrypted_base64).encode()
+
+    # Send the JSON data over the network
+    s.sendall(json_data)
+    # print("aes_encrypted",*aes_encrypted)
+
+    # Send the encrypted message to the server
+    # s.sendall(*aes_encrypted)
+
+    # Close the connection with the server
+    s.close()
